a82cb7e217ef69b68ce9acc1e98bf6562dd20dd2
fitzw <fitz.wyh@gmail.com>
2017-06-05 14:19:06 -0400

    add files

--- data_collector.py
+++ data_collector.py
@@ -1,11 +1,12 @@
 
 # coding: utf-8
 
-# In[220]:
+# In[303]:
 
 from bs4 import BeautifulSoup
 import sys
 import time
+import datetime
 import os
 import requests
 import codecs
@@ -43,19 +44,18 @@ def get_stop_info(stop_id):
 #data.keys() -> ['mode', 'stop_id', 'stop_name', 'alert_headers']
 
 
-# In[240]:
+# In[277]:
 
 def get_bus_info(stop_data,t_type,rount_n):
-    if 'error' in stop_data.keys():
-        print(stop_data)
+    if 'mode' not in stop_data.keys():
+        print('service goes offline')
     else:
         for i in stop_data['mode']:
             if i['mode_name'] == t_type:
                 for route in i['route']:
                     if route['route_name']==str(route_n):
                         return route['direction'][0]['trip']
-                else:
-                    return 'This bus is unavailable or bus id not found.'
+    return 'This bus is unavailable or bus id not found.'
 
 
 # In[147]:
@@ -68,32 +68,32 @@ def get_bus_info(stop_data,t_type,rount_n):
 #bus_data['direction'][0].keys() -> ['trip', 'direction_name', 'direction_id']
 
 
-# In[238]:
+# In[312]:
 
 #by default 
-desired_stop = 171
+desired_stop = 175
 t_type = 'Bus'
 route_n = 39
 
 
-# In[251]:
+# In[263]:
 
-parser = OptionParser()
-parser.add_option('-v', '--verbose', action='store_true')
-parser.add_option('-o', '--output-file')
-parser.add_option('-n', '--n_of_time', type = 'int')
-parser.add_option('-s', '--desired_stop', type='str')
-parser.add_option('-t', '--t_type', type='str')
-parser.add_option('-r', '--route_n', type='int')
-option, args = parser.parse_args()
-if not option.output_file:
-    print('specify input and output files')
-    parser.print_help()
-    sys.exit(1)
-if not (option.desired_stop and option.t_type and option.route_n):
-    print('specify correct input')
-    parser.print_help()
-    sys.exit(1)
+# parser = OptionParser()
+# parser.add_option('-v', '--verbose', action='store_true')
+# parser.add_option('-o', '--output-file')
+# parser.add_option('-n', '--n_of_time', type = 'int')
+# parser.add_option('-s', '--desired_stop', type='str')
+# parser.add_option('-t', '--t_type', type='str')
+# parser.add_option('-r', '--route_n', type='int')
+# option, args = parser.parse_args()
+# if not option.output_file:
+#     print('specify input and output files')
+#     parser.print_help()
+#     sys.exit(1)
+# if not (option.desired_stop and option.t_type and option.route_n):
+#     print('specify correct input')
+#     parser.print_help()
+#     sys.exit(1)
 
 
 # In[239]:
@@ -103,26 +103,43 @@ if not (option.desired_stop and option.t_type and option.route_n):
 #     t_type = sys.argv[2]
 
 
-# In[252]:
+# In[291]:
 
-import pickle
+#import pickle
 def writer(l,filename):
-    with open(filename, "wb") as fp: 
-        pickle.dump(l, fp)
+    with open(filename, "w") as fp: 
+        for item in l:
+            fp.write("%s\n" % item)
 
 
-# In[253]:
+# In[331]:
+
+from progressbar import*
+
+
+# In[332]:
 
 def worker(n,desired_stop,t_type,route_n,filename):
     count = 0
     result = []
-    while(count<0):
+    progress = ProgressBar(n, fmt=ProgressBar.FULL)
+    while(count<n):
+        progress.current += 1
+        progress()
         stop_data = get_stop_info(desired_stop)
         bus_list = get_bus_info(stop_data,t_type,route_n)
-        writer([n,bus_list],filename)
-        result.append([n,bus_list])
+        #print([count,bus_list])
+        result.append([count,datetime.datetime.now(),bus_list])
         count+=1
         time.sleep(10)
+    progress.done()
+    writer(result,filename)
+    return result
         
-worker(option.n_of_time,option.desired_stop,option.t_type, option.route_n, option.output_file)
+#worker(1800,171,'Bus',39,'test.txt')
+
+
+# In[333]:
+
+worker(3,175,'Bus',39,'t1.txt')
 
