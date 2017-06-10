e4c1c862deb2258f592e60dcd3cf235770ff4443
fitzw <fitz.wyh@gmail.com>
2017-06-10 13:43:19 -0400

    few updates

--- README.md
+++ README.md
@@ -1,15 +1,10 @@
 # rts
 Real time public transportation travel planner
 
-* n: records per file
-* stop_id: id of the stop from the file 
-* type: transportation type, Bus by default. 
-  See [stops.txt](https://github.com/fitzw/rts/blob/master/MBTA_GTFS/stops.txt)
-* route_n: transportation's route #, 39 by default
 ```
 $ python data_collector.py 
 
 Please enter parameters in format of: n, stop_id, type, route_n =>
 
-360, 175, Bus, 39
+1200, 175, Bus, 39
 ```
--- data_collector.py
+++ data_collector.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[22]:
+# In[44]:
 
 from bs4 import BeautifulSoup
 import sys
@@ -28,7 +28,7 @@ key = 'Z4Y7a0W330WT1m6DUoHXhQ'
 # 
 # 
 
-# In[241]:
+# In[56]:
 
 def get_stop_info(stop_id):
     stop = str(stop_id)
@@ -44,11 +44,15 @@ def get_stop_info(stop_id):
 #data.keys() -> ['mode', 'stop_id', 'stop_name', 'alert_headers']
 
 
-# In[277]:
+# In[87]:
+
+
+
+# In[79]:
 
 def get_bus_info(stop_data,t_type,route_n):
     if 'mode' not in stop_data.keys():
-        print('service goes offline')
+        print('service goes offline @ %d:%d'%(datetime.now().hour,datetime.now().minute))
     else:
         for i in stop_data['mode']:
             if i['mode_name'] == t_type:
@@ -102,7 +106,8 @@ def writer(l,filename):
     with open(filename+'.txt', "w") as fp: 
         for item in l:
             fp.write("%s\n" % item)
-    print(filename+' is written @ '+str(datetime.now()))
+    #print(filename+' is written @ '+str(datetime.now()))
+    return str(filename+' is written @ '+str(datetime.now()))
 
 
 # In[331]:
@@ -110,33 +115,35 @@ def writer(l,filename):
 from progressbar import*
 
 
-# In[332]:
+# In[45]:
 
-def worker(n,desired_stop,t_type,route_n,filename):
-    count = 0
-    result = []
-    progress = ProgressBar(n, fmt=ProgressBar.FULL)
-    while(count<n):
-        progress.current += 1
-        progress()
-        stop_data = get_stop_info(desired_stop)
-        bus_list = get_bus_info(stop_data,t_type,route_n)
-        #print([count,bus_list])
-        result.append([count,datetime.now(),bus_list])
-        count+=1
-        tm.sleep(10)
-    progress.done()
-    writer(result,filename)
-    return result
+# def worker(n,desired_stop,t_type,route_n,filename):
+#     count = 0
+#     result = []
+#     progress = ProgressBar(n, fmt=ProgressBar.FULL)
+#     while(count<n):
+#         progress.current += 1
+#         progress()
+#         stop_data = get_stop_info(desired_stop)
+#         bus_list = get_bus_info(stop_data,t_type,route_n)
+#         #print([count,bus_list])
+#         result.append([count,datetime.now(),bus_list])
+#         count+=1
+#         tm.sleep(10)
+#     progress.done()
+#     writer(result,filename)
+#     return result
         
-#worker(1800,171,'Bus',39,'test.txt')
+# #worker(1800,171,'Bus',39,'test.txt')
 
 
-# In[33]:
+# In[55]:
 now = datetime.now()
 now_time = now.time()
 n,desired_stop, t_type, route_n = \
-    input("Please enter parameters in format of: n, stop_id, type, route_n =>").split(',')
+                    input("Please enter parameters in format of: n, stop_id, type, route_n =>").split(',')
+
+#360,175,'Bus',39
 count = 0
 while(True):
     if (time(4,30) <= now.time() <= time(23,59)) or (time(0,0) <= now.time() <= time(1,0)):
@@ -156,7 +163,10 @@ while(True):
             count+=1
             tm.sleep(10)
         progress.done()
-        writer(result,saved_) 
+        print('Data_%d collected, writting to file: ' %(count//int(n)-1)+saved_ )
+        msg = writer(result,saved_)
+        if msg:
+            print(msg)
 # In[32]:
 
 # #by default 
